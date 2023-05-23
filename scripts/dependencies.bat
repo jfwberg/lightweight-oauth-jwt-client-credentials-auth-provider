99498cf64a2ac3b3d87c38b24b4df33fa09ddab5
Justus van den Berg <jfwberg@gmail.com>
2023-05-23 14:31:37 +0100

    progress

---
+++ scripts/dependencies.bat
@@ -0,0 +1,2 @@
+rem Install dependency on package "Lightweight - Auth Provider Util v2@0.1.0-1" - /packaging/installPackage.apexp?p0=04t4K000002JuxSQAS
+sf package install -p "04t4K000002JuxSQAS" -w 30
--- scripts/package.bat
+++ scripts/package.bat
@@ -1,8 +1,8 @@
 rem CREATE A PACKAGE - UPDATE DEVHUB
-sf package create --name "OAuth 2.0 JWT Client Credentials Authentication Auth Provider" --description "A generic Auth Provider Apex class that can be used with named/external credentials to get an access token using the OAuth 2.0 JWT Client Credentials Authentication Flow." --package-type "Managed" --path "force-app" --target-dev-hub "[DEVHUB NAME]"
+sf package create --name "OAuth 2.0 JWT Client Credentials Authentication Auth Provider" --description "A lightweight generic Auth Provider Apex class that can be used with named/external credentials to get an access token using the OAuth 2.0 JWT Client Credentials Authentication Flow." --package-type "Managed" --path "force-app" --target-dev-hub "[DEVHUB NAME]"
 
 rem CREATE A PACKAGE VERSION - UPDATE DEVHUB
-sf package version create --package "OAuth 2.0 JWT Client Credentials Authentication Auth Provider" --installation-key-bypass --code-coverage --target-dev-hub  "[DEVHUB NAME]" -w 30
+sf package version create --package "Lightweight - OAuth 2.0 JWT Client Credentials Auth Provider" --installation-key-bypass --code-coverage --target-dev-hub  "[DEVHUB NAME]" -w 30
 
 rem PROMOTE THE PACKAGE VERSION - UPDATE NAME + DEVHUB
-sf package version promote --package "OAuth 2.0 JWT Client Credentials Authentication Auth Provider@0.1.0-2" --target-dev-hub  "[DEVHUB NAME]"
+sf package version promote --package "Lightweight - OAuth 2.0 JWT Client Credentials Auth Provider@0.1.0-1" --target-dev-hub  "[DEVHUB NAME]"
